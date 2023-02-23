722f78c77efb46fc3d5b03f1c0294dd59fdfaa6c
Balint Horvath <horvath.balint997@gmail.com>
2023-02-23 11:38:00 +0100

    hosting on firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HackereffectComponent } from './hackereffect/hackereffect.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBs0FmL75Xgi6DbdZbotLbRorSJqGetssc',
+  authDomain: 'bvlint-sandbox.firebaseapp.com',
+  projectId: 'bvlint-sandbox',
+  storageBucket: 'bvlint-sandbox.appspot.com',
+  messagingSenderId: '834938609327',
+  appId: '1:834938609327:web:22e4be4b0fa22ad176be5a',
+  measurementId: 'G-9DMBHFG28W',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @NgModule({
   declarations: [AppComponent, HackereffectComponent],
