90b856fb66e5340fd40ee5f708e7bf54b7ba4ef6
Balint Horvath <horvath.balint997@gmail.com>
2023-03-13 10:56:12 +0100

    renderer changes

--- src/app/threejsrenderer/threejsrenderer.component.ts
+++ src/app/threejsrenderer/threejsrenderer.component.ts
@@ -22,7 +22,7 @@ export class ThreejsrendererComponent implements AfterViewInit {
   ngAfterViewInit() {
     this.container = this.containerRef.nativeElement;
     this.initScene();
-    this.loadModel('assets/scene.gltf');
+    this.loadModel('assets/lancia/scene.gltf');
     this.render();
   }
 
@@ -35,6 +35,23 @@ export class ThreejsrendererComponent implements AfterViewInit {
     this.camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
     this.camera.position.z = 5;
     this.camera.position.y = 1.3;
+
+    // Rotate the model
+    if (this.scene.children.length > 0) {
+      const model = this.scene.children[0];
+      model.rotation.y += 0.005;
+    }
+
+    // Create the spotlight
+    const spotLight = new THREE.SpotLight(0xffffff, 4);
+    spotLight.position.set(100, 1000, 100);
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+    spotLight.shadow.camera.near = 500;
+    spotLight.shadow.camera.far = 4000;
+    spotLight.shadow.camera.fov = 30;
+    this.scene.add(spotLight);
 
     // Create the renderer
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -65,8 +82,8 @@ export class ThreejsrendererComponent implements AfterViewInit {
 
     // Rotate the model
     if (this.scene.children.length > 0) {
-      const model = this.scene.children[0];
-      model.rotation.y += 0.005;
+      // const model = this.scene.children[0];
+      this.scene.rotation.y += 0.005;
     }
 
     this.renderer.render(this.scene, this.camera);
