9e389f067f9f6a457b8995e027fa815095621b9c
Bálint Horváth <horvath.balint997@gmail.com>
2023-03-24 13:19:13 +0100

    blogposts with strapi init

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,15 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
 })
 export class BlogComponent implements OnInit {
+  blogposts = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.fetchBlogposts();
+
+    console.log(this.blogposts);
   }
 
+  fetchBlogposts() {
+    this.http
+      .get('http://localhost:1337/api/blogs')
+      .subscribe((data: any) => (this.blogposts = data.data));
+  }
 }
