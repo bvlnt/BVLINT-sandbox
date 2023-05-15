b2f587c024884a091ca60dc5c2921a02d345f611
Bálint Horváth <horvath.balint997@gmail.com>
2023-05-15 15:43:55 +0200

    mars rover image

--- src/app/components/spacedashboard/spacedashboard.component.ts
+++ src/app/components/spacedashboard/spacedashboard.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Apod } from './spacedashboard.interface';
+import { Apod } from '../../interfaces/spacedashboard.interface';
 
 @Component({
   selector: 'app-spacedashboard',
@@ -10,20 +10,39 @@ import { Apod } from './spacedashboard.interface';
 export class SpacedashboardComponent implements OnInit {
   Apod: Apod | undefined;
 
+  private apiUrl =
+    'https://api.nasa.gov/mars-photos/api/v1/rovers/Curiosity/latest_photos?api_key=uK5HH7WWn22qEfk2Q7NZTFqD49yk7alwesPM3T3k';
+
+  imageUrl: string | undefined;
+  earthDate: string | undefined;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getAPOD();
+    this.getLatestPhoto();
   }
 
   getAPOD() {
+    const apiKey = 'uK5HH7WWn22qEfk2Q7NZTFqD49yk7alwesPM3T3k';
     this.http
-      .get(
-        'https://api.nasa.gov/planetary/apod?api_key=uK5HH7WWn22qEfk2Q7NZTFqD49yk7alwesPM3T3k'
-      )
+      .get('https://api.nasa.gov/planetary/apod?api_key=${apiKey}')
       .subscribe((response) => {
         this.Apod = response as Apod;
         console.log(this.Apod);
       });
+  }
+
+  getLatestPhoto() {
+    this.http.get(this.apiUrl).subscribe((data: any) => {
+      const navCamPhotos = data.latest_photos.filter(
+        (photo: { camera: { name: string } }) => photo.camera.name === 'NAVCAM'
+      );
+      if (navCamPhotos.length > 0) {
+        const latestNavCamPhoto = navCamPhotos[0];
+        this.imageUrl = latestNavCamPhoto.img_src;
+        this.earthDate = latestNavCamPhoto.earth_date;
+      }
+    });
   }
 }
