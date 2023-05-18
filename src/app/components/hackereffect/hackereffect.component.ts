288776862461e41091c448f90a8a75d6c0f5cbec
Bálint Horváth <horvath.balint997@gmail.com>
2023-05-18 13:21:41 +0200

    gsap animations

--- src/app/components/hackereffect/hackereffect.component.ts
+++ src/app/components/hackereffect/hackereffect.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core';
 import { fromEvent, interval } from 'rxjs';
 import { map, takeWhile } from 'rxjs/operators';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-hackereffect',
@@ -20,9 +21,23 @@ export class HackereffectComponent implements OnInit, AfterViewInit {
     const letters =
       'abcdefghijklmnopqrstuvwxyz1234567890.,-*<>äđĐ[|Äˇ^~˘]{}()@&#łí˙`ł◙⠕⊹⑈☗ ∰ ፨ ☼ ⑇⊹⍜⤿☼╫§╜φ°⌂▌⛢٥≈ტ◊◙⠕⊹⑈☗ ∰ ፨ ☼ ⑇⊹⍜⤿☼╫§╜φ°⌂▌⛢٥≈ტ◊◙⠕⊹⑈☗ ∰ ፨ ☼ ⑇⊹⍜⤿☼╫§╜φ°⌂▌⛢٥≈ტ◊';
     const titleEl = this.title.nativeElement;
+    const desc = document.querySelector('#desc');
     const targetText = titleEl.dataset.value;
 
     const mouseover$ = fromEvent(titleEl, 'mouseover');
+
+    const titleAnimation = gsap.from(titleEl, {
+      opacity: 0,
+      x: -200,
+      duration: 1,
+      ease: 'power3.in',
+    });
+    const descAnimation = gsap.from(desc, {
+      opacity: 0,
+      x: -200,
+      duration: 2,
+      ease: 'power3.in',
+    });
 
     const effect$ = mouseover$.pipe(
       map(() => {
