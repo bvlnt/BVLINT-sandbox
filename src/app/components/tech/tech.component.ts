c84a9bac8733d1b337af3cd31713af03750cd11f
Bálint Horváth <horvath.balint997@gmail.com>
2023-05-20 16:41:15 +0200

    gsap import as script

--- src/app/components/tech/tech.component.ts
+++ src/app/components/tech/tech.component.ts
@@ -1,4 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import Draggable from 'gsap/Draggable';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-tech',
@@ -18,3 +21,41 @@ export class TechComponent implements OnInit, AfterViewInit {
     });
   }
 }
+
+//WORKING GSAP SCROLLTRIGGER:
+
+// import { Component, HostListener, OnInit } from '@angular/core';
+// import { gsap } from 'gsap';
+// import Draggable from 'gsap/Draggable';
+// import ScrollTrigger from 'gsap/ScrollTrigger';
+
+// @Component({
+//   selector: 'my-app',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+// })
+// export class AppComponent implements OnInit {
+//   constructor() {}
+
+//   ngOnInit() {
+//     gsap.registerPlugin(ScrollTrigger, Draggable);
+
+//     this.initScrollTriggers();
+//   }
+
+//   initScrollTriggers() {
+//     document.querySelectorAll('.box').forEach((box) => {
+//       const scrollBox = gsap.timeline({
+//         scrollTrigger: {
+//           trigger: box,
+//           pin: true,
+//           start: 'top top',
+//           end: 'bottom bottom',
+//           markers: true,
+//           toggleActions: 'play none none reverse',
+//         },
+//       });
+//       scrollBox.from(box, { y: 30, opacity: 0 });
+//     });
+//   }
+// }
